9c7c35a31473de006ed06042f937817bfa17b7c7
SHawkeye77 <samwhataguy@gmail.com>
2019-08-06 21:11:55 -0500

    added room descriptions for main dome (some)

--- tiles_all/tiles_main_dome.py
+++ tiles_all/tiles_main_dome.py
@@ -8,8 +8,8 @@ class ApmTerminalB(Location):
     def __init__(self, x, y):
         super().__init__(x=x, y=y, name="APM Terminal B",
                          connected=["HallA"],
-                         description="The APM track runs the length of the West end of the room. There's benches and a "
-                                     "waiting area to the East where a tablet sits.",
+                         description="The APM track runs the length of the West end of the room. There are benches and "
+                                     "a waiting area to the East where a tablet sits.",
                          items=[items.Tablet()])
 
 
@@ -27,7 +27,7 @@ class Booth1(Location):
         super().__init__(x=x, y=y, name="Booth 1",
                          connected=["SpiritualCenter"],
                          description="A cramped, soundproof booth barely big enough to fit a small bench and a little "
-                                     "leg-room. It's no larger than your bedroom closet back on terra.")
+                                     "leg-room. It's no larger than your bedroom closet back on Terra.")
 
 
 class Booth2(Location):
@@ -35,14 +35,14 @@ class Booth2(Location):
         super().__init__(x=x, y=y, name="Booth 2",
                          connected=["SpiritualCenter"],
                          description="A cramped, soundproof booth barely big enough to fit a small bench and a little "
-                                     "leg-room. It's no larger than your bedroom closet back on terra.")
+                                     "leg-room. It's no larger than your bedroom closet back on Terra.")
 
 
 class Bathrooms(Location):
     def __init__(self, x, y):
         super().__init__(x=x, y=y, name="Bathrooms",
                          connected=["HallC"],
-                         description="A clean bathroom with a bright white finish. There's five stalls "
+                         description="A clean bathroom with a bright white finish. There are five stalls "
                                      "and a hallway cutting through the center of the room. At the end is a single "
                                      "sink and full length mirror.",
                          items=[items.Mirror()])
@@ -60,42 +60,53 @@ class Cafeteria(Location):
     def __init__(self, x, y):
         super().__init__(x=x, y=y, name="Cafeteria",
                          connected=["HallE"],
-                         description="CAFE DESCRIPTION HERE")
+                         description="Scattered about the room are tables of a variety of shapes and sizes. "
+                                     "On the far wall is a large cyan tile with a black handle. Beneath it are"
+                                     "four levers labeled 'B', 'L', 'D', and 'S'.")
 
 
 class Bar(Location):
     def __init__(self, x, y):
         super().__init__(x=x, y=y, name="Bar",
                          connected=["HallF"],
-                         description="BAR DESCRIPTION HERE")
+                         description="A dimly lit room complete with a bar, red velvet booths, and an old "
+                                     "Steinway piano. Speakers hang from the ceiling above a black-and-white "
+                                     "checkerboard dance floor. You half expect Frank Sinatra to walk in.")
 
 
 class MovieTheater(Location):
     def __init__(self, x, y):
         super().__init__(x=x, y=y, name="Movie Theater",
                          connected=["HallH"],
-                         description="MOVIE THEATER DESCRIPTION HERE")
+                         description="The east wall is completely taken up by a massive digital screen. Facing "
+                                     "the screen are rows of seats, separated into two sections by a narrow aisle. "
+                                     "At the back is a projector. Some discs lay neatly beneath it.")
 
 
 class MaintenanceRoom(Location):
     def __init__(self, x, y):
         super().__init__(x=x, y=y, name="Maintenance Room",
                          connected=["HallI"],
-                         description="MAINTENANCE ROOM DESCRIPTION HERE")
+                         description="MAINTENANCE ROOM DESCRIPTION HERE")  #SKIPPED ===================================================
 
 
 class TerranCommodityStore(Location):
     def __init__(self, x, y):
         super().__init__(x=x, y=y, name="Terran Commodity Store",
                          connected=["ShoppingCenter"],
-                         description="TERRAN COMMODITY STORE DESCRIPTION HERE")
+                         description="The store is divided into three sections: A jewelry counter with glass-"
+                                     "enclosed trinkets, a well-stocked bookshelf labeled \"Terran Classics\", and "
+                                     "a clothing section with luxury apparel.")
 
 
 class ShoppingCenter(Location):
     def __init__(self, x, y):
         super().__init__(x=x, y=y, name="Shopping Center",
                          connected=["HallJ"],
-                         description="SHOPPING CENTER DESCRIPTION HERE")
+                         description="It's one of the largest rooms at the base. The shelves contain everything from "
+                                     "canned goods to space-travel staples like rice and beans. There's even a "
+                                     "small section of drugs and pharmaceutical products at the back. A pay station "
+                                     "guards the exit.")
 
 
 class SecurityCenter(Location):
